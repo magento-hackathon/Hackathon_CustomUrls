b5064b82d914fd132f381dad103bba4ab0583b9f
Marco De Bortoli <marco.debo.debortoli@gmail.com>
2013-09-18 15:59:46 +0200

    Test refactored to provide a meaningful feature definition

--- src/app/code/community/Hackathon/CustomUrls/Test/Config/Main.php
+++ src/app/code/community/Hackathon/CustomUrls/Test/Config/Main.php
@@ -2,12 +2,20 @@
 
 class Hackathon_CustomUrls_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config
 {
-    public function testClassAlias()
+    /**
+     * @test
+     * @group config
+     */
+    public function it_properly_resolves_the_model_alias()
     {
         $this->assertModelAlias('hackathon_customurls/observer', 'Hackathon_CustomUrls_Model_Observer');
     }
 
-    public function testEventObserver()
+    /**
+     * @test
+     * @group config
+     */
+    public function it_defines_the_required_custom_obeserver()
     {
         $this->assertEventObserverDefined(
             'global',
